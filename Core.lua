65ce6b949d94125385d8559ae08d13c14f7e1009
Chev <Chev@ChevCast.tv>
2024-09-05 14:17:26 -0600

    Temporarly committing this. Will drop this commit when I come back.

--- Core.lua
+++ Core.lua
@@ -3,6 +3,32 @@ QuestTogether =
 local AceConfig = LibStub("AceConfig-3.0")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 
+function ClearAllActionBars()
+	-- Iterate over all action bars
+	for i = 1, NUM_ACTIONBAR_BUTTONS do
+		-- Clear each action bar
+		ClearActionBar(i)
+	end
+
+	-- ElvUI adds extra action bars, so we need to clear those as well
+	local numExtraBars = GetNumShapeshiftForms()
+	for i = 1, numExtraBars do
+		-- Clear each extra action bar
+		ClearActionBar(i + NUM_ACTIONBAR_BUTTONS)
+	end
+end
+
+function ClearActionBar(bar)
+	-- Iterate over all buttons on the action bar
+	for i = 1, 12 do
+		-- Get the action ID for the button
+		local actionID = (bar - 1) * 12 + i
+		-- Clear the action
+		PickupAction(actionID)
+		ClearCursor()
+	end
+end
+
 function QuestTogether:Debug(message)
 	if self.db.profile.debugMode then
 		self:Print("Debug: " .. message)
